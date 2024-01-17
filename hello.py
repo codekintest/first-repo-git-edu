d6ae68618243d412ac460f05cbe49b44ed698868
codekintest <156533048+codekintest@users.noreply.github.com>
2024-01-17 19:11:18 +0000

    update

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ def saludar(name):
     return f"Hola {name} bienvenido!"
 
 
-name = input("¿Dime cual es tu nombre?")
+name = input("¿Dime cual es tu nombre?\n")
 
 if __name__ == "__main__":
     print(saludar(name))
