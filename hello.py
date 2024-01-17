41cbf706ffc5ac0e6cfad0744c577dc5be8cfae9
codekintest <156533048+codekintest@users.noreply.github.com>
2024-01-17 19:08:39 +0000

    Agrega input del nombre

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("Hello, git")
+
+
+def saludar(name):
+    return f"Hola {name} bienvenido!"
+
+
+name = input("¿Dime cual es tu nombre?")
+
+if __name__ == "__main__":
+    print(saludar(name))
