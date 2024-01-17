b742b4ad18a76a08064dd55c6eb85b416117625c
codekintest <156533048+codekintest@users.noreply.github.com>
2024-01-17 19:03:17 +0000

    Cambio nombre repositorio

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# repo-git-edu
+# first-repo-git-edu
 Este un repositorio educativo sobre el uso Git y GitHub
--- hello.py
+++
@@ -1,9 +0,0 @@
-print("Hello, git")
-
-
-def saludar(name):
-    return f"Hola {name} bienvenido!"
-
-
-if __name__ == "__main__":
-    print(saludar("Elkin"))
